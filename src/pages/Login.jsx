0ea8b59821a90649a427228acfff8c9a55e1a0a1
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-13 16:30:25 +0000

    Login page created.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoImg from '../img/logo.jpg';
+import { Card, Logo, Form, Input, Button } from '../components/AuthForms';
+
+const Login = () => (
+  <Card>
+    <Logo src={logoImg} />
+    <Form>
+      <Input type="email" placeholder="email" />
+      <Input type="password" placeholder="password" />
+      <Button>Sign In</Button>
+    </Form>
+    <Link to="/signup">Don&apos;t have an account?</Link>
+  </Card>
+);
+
+export default Login;
