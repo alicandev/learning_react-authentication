e7a232042477ce024792471041ae4335ec28d677
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-13 15:47:53 +0000

    Simple Admin and Home pages, and a private route template has been created

---
+++ src/pages/Admin.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Admin = (props) => (
+  <div>Admin Page</div>
+)
+
+export default Admin
---
+++ src/privateRoute.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    <Component {...props} />
+  )} />
+)
+
+PrivateRoute.propTypes = {
+  component: PropTypes.component
+}
+
+export default PrivateRoute
